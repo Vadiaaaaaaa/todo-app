1b032070bc4907caf4c9a483320a66de8c21a153
Vedika Saboo <112643576+Vadiaaaaaaa@users.noreply.github.com>
2025-06-27 16:59:40 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // ✅ makes all paths relative so Netlify can load JS, CSS, images correctly
   plugins: [react()],
 })
-
