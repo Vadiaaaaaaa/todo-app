5543d2eacd650a91091e75584455df9bf8767022
Vedika Saboo <vedikasaboo12@gmail.com>
2025-06-27 15:37:34 +0530

    Set base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // this helps load assets correctly on GitHub Pages
+  base: '/todo-app/', 
   plugins: [react()],
 })
